f52faa7d7040ec72c1788ac361c0b660f70ff428
VDC\tnguyen450 <tnguyen450@csc.com>
2018-01-04 17:51:13 +0700

    commonsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
     context: path.resolve('js'),
